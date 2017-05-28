b3ae7490688a64fd366b71d4f313dbc0c2c4891d
李婉 <iwan16406783@gmail.com>
2017-05-28 11:02:25 +0800

    implement search

--- db/schema.rb
+++ db/schema.rb
@@ -21,11 +21,8 @@ ActiveRecord::Schema.define(version: 20170515151205) do
   end
 
   create_table "carts", force: :cascade do |t|
-    t.integer  "cart_id"
-    t.integer  "product_id"
-    t.integer  "quantity",   default: 1
-    t.datetime "created_at",             null: false
-    t.datetime "updated_at",             null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "orders", force: :cascade do |t|
