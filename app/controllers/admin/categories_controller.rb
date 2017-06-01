8936809299be4146c72298dd3f85feb05344a98a
hyacinth0906 <hyacinthyang0906@gmail.com>
2017-06-01 23:18:38 +0800

    小错误

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,56 +1,51 @@
 class Admin::CategoriesController < ApplicationController
 
-    layout "admin"
-    before_action :authenticate_user!
-    before_action :admin_required
+  layout "admin"
 
-    # GET /categories
-    # GET /categories.json
-    def index
-      @categories = Category.all
-    end
+  before_action :authenticate_user!
+  before_action :admin_required
 
-    def new
-      @category = Category.new
-    end
-
-    def create
-      @category = Category.new(category_params)
-      if @category.save
-        redirect_to admin_categories_path
-      else
-        render :new
-      end
-    end
-
-    def edit
-      @category = Category.find(params[:id])
-    end
-
-    def update
-      @category = Category.find(params[:id])
-      if @category.update(category_params)
-        redirect_to admin_category_path
-      else
-        render :edit
-      end
-    end
-
-    def destroy
-      @category = Category.find(params[:id])
-      @category.destroy
-      redirect_to admin_categories_path, warning: "你已删除这个分类"
-    end
-
-
-    private
-      # Use callbacks to share common setup or constraints between actions.
-
-
-      # Never trust parameters from the scary internet, only allow the white list through.
-      def category_params
-        params.require(:category).permit(:name)
-      end
+  def index
+    @categories = Category.all
   end
 
+  def new
+    @category = Category.new
+  end
+
+
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      redirect_to admin_categories_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @category = Category.find(params[:id])
+  end
+
+  def update
+    @category = Category.find(params[:id])
+    if @category.update(category_params)
+      redirect_to admin_categories_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @category = Category.find(params[:id])
+    @category.destroy
+    redirect_to admin_categories_path, warning: "你已删除这个分类"
+  end
+
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name)
+  end
 end
