889d1f9848668349072279d317f7c5ddb2caa974
李婉 <iwan16406783@gmail.com>
2017-06-01 11:43:51 +0800

    correct category

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170529032907) do
+ActiveRecord::Schema.define(version: 20170601033910) do
 
   create_table "cart_items", force: :cascade do |t|
     t.integer  "cart_id"
@@ -21,11 +21,8 @@ ActiveRecord::Schema.define(version: 20170529032907) do
   end
 
   create_table "carts", force: :cascade do |t|
-    t.integer  "cart_id"
-    t.integer  "product_id"
-    t.integer  "quantity",   default: 1
-    t.datetime "created_at",             null: false
-    t.datetime "updated_at",             null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "categories", force: :cascade do |t|
@@ -67,6 +64,7 @@ ActiveRecord::Schema.define(version: 20170529032907) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.string   "image"
+    t.integer  "category_id"
   end
 
   create_table "users", force: :cascade do |t|
