48bafb0d5db217e003096f0d8fabd0958982839f
hyacinth0906 <hyacinthyang0906@gmail.com>
2017-06-07 00:51:24 +0800

    增加购买须知及修改首页，修复七牛云

--- config/routes.rb
+++ config/routes.rb
@@ -48,5 +48,7 @@ Rails.application.routes.draw do
     resources :orders
   end
 
+    get "/pages/about" => "pages#about"
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
